43b2b133deec86663a933f343d3bd48f9f0ad967
Hasby Rosyadi <hasbyrosyadi1995@gmail.com>
2017-11-30 18:03:26 +0700

    hasby

---
+++ TugasAkhirConsumer/src/main/java/com/example/demo/DAO/mahasiswaMapper.java
@@ -0,0 +1,20 @@
+package com.example.demo.DAO;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Update;
+
+import com.example.demo.Model.Mahasiswa;
+
+@Mapper
+public interface mahasiswaMapper {
+
+	@Insert("Insert into mahasiswa (nama, npm, is_dapat_lulus, ip, ipk, sks_total, id_univ, id_fakultas, id_program_studi) "
+			+ "VALUES (#{nama}, #{npm}, 0, 0.00, 0.00, 0, #{id_univ}, #{id_fakultas}, #{id_program_studi})")
+	void add(Mahasiswa mahasiswa);
+	
+	@Update("Update mahasiswa SET nama=#{nama}, npm=#{npm}, id_univ=#{id_univ}, id_fakultas=#{id_fakultas}, id_program_studi="
+			+ "#{id_program_studi}  ")
+	void update(Mahasiswa mahasiswa);
+	
+}
--- TugasAkhirProducer/src/main/java/com/example/demo/DatabaseService/MahasiswaServiceDatabase.java
+++ TugasAkhirProducer/src/main/java/com/example/demo/DatabaseService/MahasiswaServiceDatabase.java
@@ -1,8 +1,53 @@
 package com.example.demo.DatabaseService;
 
+import com.example.demo.DAO.MahasiswaDAO;
+import com.example.demo.Model.Mahasiswa;
 import com.example.demo.Service.MahasiswaService;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MahasiswaServiceDatabase implements MahasiswaService {
+
+	@Autowired
+	MahasiswaDAO mahasiswaDao;
+	
+	@Override
+	public List<Mahasiswa> selectAllMahasiswa() {
+		// TODO Auto-generated method stub
+		return mahasiswaDao.mahasiswa();
+	}
+
+	@Override
+	public void addMahasiswa(Mahasiswa mahasiswa) {
+		// TODO Auto-generated method stub
+		mahasiswaDao.add(mahasiswa);
+	}
+
+	@Override
+	public void updateMahasiswa(Mahasiswa mahasiswa) {
+		// TODO Auto-generated method stub
+		mahasiswaDao.update(mahasiswa);
+	}
+
+	@Override
+	public void deleteMahasiswa(Mahasiswa mahasiswa) {
+		// TODO Auto-generated method stub
+		mahasiswaDao.delete(mahasiswa);
+	}
+
+	@Override
+	public Mahasiswa lulus(String lulus) {
+		// TODO Auto-generated method stub
+		return mahasiswaDao.lulus(lulus);
+	}
+
+	@Override
+	public Mahasiswa dataView(String npm) {
+		// TODO Auto-generated method stub
+		return mahasiswaDao.dataView(npm);
+	}
 }
