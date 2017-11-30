3f230e0bc2c4a15cd4c47486e770368a5c51b5e6
Wendy1997 <wendydamar.wb@gmail.com>
2017-11-30 18:23:32 +0700

    conflict fix

--- TugasAkhirConsumer/src/main/java/com/example/demo/TugasAkhirConsumerApplication.java
+++ TugasAkhirConsumer/src/main/java/com/example/demo/TugasAkhirConsumerApplication.java
@@ -2,10 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-<<<<<<< HEAD
 import org.springframework.boot.web.client.RestTemplateBuilder;
-=======
->>>>>>> wendybaru
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
