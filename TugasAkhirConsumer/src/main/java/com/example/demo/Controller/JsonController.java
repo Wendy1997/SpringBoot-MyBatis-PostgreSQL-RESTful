0cc58db707edb4b5989dc093fae78c5cbea182b9
Wendy1997 <wendydamar.wb@gmail.com>
2017-12-06 12:13:39 +0700

    resove conflict

--- TugasAkhirConsumer/src/main/java/com/example/demo/Controller/JsonController.java
+++ TugasAkhirConsumer/src/main/java/com/example/demo/Controller/JsonController.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.example.demo.Controller;
 
 import com.example.demo.Model.Kurikulum;
@@ -29,35 +28,3 @@ public class JsonController {
         return "tesAPI";
     }
 }
-=======
-package com.example.demo.Controller;
-
-import com.example.demo.Model.Kurikulum;
-import com.example.demo.Model.Mahasiswa;
-import com.example.demo.Model.MataKuliah;
-import com.example.demo.Service.MataKuliahService;
-import com.google.gson.Gson;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
-
-@Controller
-public class JsonController {
-
-    @Autowired
-    MataKuliahService mataKuliahService;
-
-    @GetMapping("/json")
-    public String getJson(Model model){
-        Gson gson = new Gson();
-
-        Kurikulum tes = mataKuliahService.getKurikulum("1", "1","1","1");
-        String json = gson.toJson(tes);
-        model.addAttribute("json", json);
-        return "tesAPI";
-    }
-}
->>>>>>> login
